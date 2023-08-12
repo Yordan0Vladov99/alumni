92adc2e5aebc7301435d646dde4acaccac975eb7
Yordan Vladov <yordan_vladov@abv.bg>
2023-08-12 15:41:29 +0300

    fixed footer alignment

--- code/alumni-front/src/components/Login/Login.tsx
+++ code/alumni-front/src/components/Login/Login.tsx
@@ -7,8 +7,10 @@ function Login() {
       <div className="call-to-action">
         <h1>Share your memories</h1>
         <p>
-          With our platform you can save your graduation photos in different forms, allowing you
-          to treasure your graduation exactly how you want to. Future alumni can also request photo sessions from our top photographers.
+          With our platform you can save your graduation photos in different
+          forms, allowing you to treasure your graduation exactly how you want
+          to. Future alumni can also request photo sessions from our top
+          photographers.
         </p>
       </div>
       <div className="form-section">
